7a4441a53e9e47b0301ed64910749cd7138108be
cowkao <cowkaoa@126.com>
2022-09-16 20:58:44 +0800

    add codes

---
+++ build/precache-manifest.b42833ab960dd608cd227491b86ce32d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421201eae080270c3aa7fbb3cfe9d6b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db4ef159d7191d121c75",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "1428dd24a3553bbab2f7",
+    "url": "./static/css/main.3830a766.chunk.css"
+  },
+  {
+    "revision": "db4ef159d7191d121c75",
+    "url": "./static/js/2.5ff87f69.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.5ff87f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1428dd24a3553bbab2f7",
+    "url": "./static/js/main.197df3ef.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "09d98d9603285fce96c458e9c44d9323",
+    "url": "./static/media/box.09d98d96.png"
+  }
+]);
